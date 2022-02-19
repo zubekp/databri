98c0d6db859738c351802ad885a2238f3491be80
petr.zubek@seznam.cz <petr.zubek@seznam.cz>
2022-02-19 15:49:14 +0000

    added new file test_tree

---
+++ test_tree.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+class Demo:
+    
+    def __init__(self,name='Petr',age=47):
+        self.name=name
+        self.age=age
+        
+    def display_name(self):
+        print(self.name,'-',age)
+
+# COMMAND ----------
+
+
