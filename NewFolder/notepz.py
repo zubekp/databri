a743f5c19712871e5506fb0b067be7c8a6805eb1
Petr Z <petr.zubek@seznam.cz>
2022-02-20 18:02:36 +0100

    Update notepz.py

--- NewFolder/notepz.py
+++ NewFolder/notepz.py
@@ -11,8 +11,28 @@ class MyRecord:
 
 # COMMAND ----------
 
+<<<<<<< HEAD
 listek = []
 
 # COMMAND ----------
 
 print('no otot')
+=======
+listek = [MyRecord('Petere',i) for i in range(5)]
+
+# COMMAND ----------
+
+print(listek)
+
+# COMMAND ----------
+
+print(listek[3].age)
+
+# COMMAND ----------
+
+print(listek[3].name, listek[4].age)
+
+# COMMAND ----------
+
+
+>>>>>>> parent of 7fbdff6 (delete to back)
